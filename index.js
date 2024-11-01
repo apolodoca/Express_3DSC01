a0fac6837c03323ade563ec77db32cba56fb4e22
Apolo Doca <89225317+apolodoca@users.noreply.github.com>
2024-11-01 15:09:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3303;
+
+app.use("/public", express.static);
+
+app.get("/contato", function (req, res) {
+  res.sendFile(__dirname + "/src/views/contato.html");
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+// ROTA UTILIZANDO MIDDLEWARE(404)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
+
+app.listen(PORT, () => {
+  console.log("running...");
+});
